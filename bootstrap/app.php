28f5b0b37a744fb263b09e98df9dd04619a47a7d
brahim bazi <139265795+brahim77777@users.noreply.github.com>
2024-05-27 18:41:39 +0100

    secondary_images

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Middleware\admin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -16,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->alias(['admin' => admin::class]);
+
+
 
         //
     })
